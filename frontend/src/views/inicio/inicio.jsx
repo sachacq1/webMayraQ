1c8e95c62137295d623f6db909a003c95c576078
sachacq1 <sachacq1@gmail.com>
2025-03-24 19:50:09 -0300

    cambios en responsivo

--- frontend/src/views/inicio/inicio.jsx
+++ frontend/src/views/inicio/inicio.jsx
@@ -5,17 +5,19 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const Inicio = () => {
     return (< >
-        <div className="container d-flex flex-column flex-md-row justify-content-center align-items-center text-center text-lg-start"
-            style={{ height: '100vh' }}>
+        <div className="container  d-flex flex-column flex-md-row justify-content-center align-items-md-center text-center text-md-start gap-4"
+            style={{
+                minHeight: '100vh',
+            }}>
 
-            <div className="col md-7 justify-content-center m-auto   ">
+            <div className="col md-7 d-flex flex-column justify-content-center align-items-center ">
                 <h1 className="fw-bold">
                     <span className="text-danger  fs-1">Bienestar emocional </span> para una vida plena</h1>
                 <p>Psicóloga clínica especializada en terapia individual, de pareja y manejo del estrés.
                     Te acompaño en tu proceso de crecimiento personal con un enfoque integrador y
                     personalizado.</p></div>
 
-            <div className="col-md-5 mt-4 mt-lg-0 d-flex align-items-center">
+            <div className="col-md-5 d-flex justify-content-center">
                 <img src="/photo2.png" alt="imagen de psi" className="img-fluid col md-5" />
             </div>
         </div>
