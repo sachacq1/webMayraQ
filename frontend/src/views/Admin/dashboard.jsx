823947d81bf7da902c5d080bc707bddc0c43c4ab
sachacq1 <sachacq1@gmail.com>
2025-04-09 12:03:21 -0300

    cambio dashb

--- frontend/src/views/Admin/dashboard.jsx
+++ frontend/src/views/Admin/dashboard.jsx
@@ -102,7 +102,13 @@ const AdminPacientesList = () => {
                                 </h5>
                                 <p className="card-text"><strong>DNI:</strong> {paciente.dni}</p>
                                 <div className="d-flex justify-content-end gap-2">
-                                    <button className="btn btn-warning btn-sm" onClick={() => handleEdit(paciente)}>Editar</button>
+                                    <Link
+                                        to={`/pacientes/editar/${paciente._id}`}
+                                        className="button is-warning"
+                                    >
+                                        Editar
+                                    </Link>
+
                                     <button className="btn btn-danger btn-sm" onClick={() => handleDelete(paciente._id)}>Eliminar</button>
                                 </div>
                             </div>
