d091edb747895943115602027be47573c664c4eb
sachacq1 <sachacq1@gmail.com>
2025-04-09 09:19:26 -0300

    cambio en main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppRouter } from "./router/Router.jsx";
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/authContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <AppRouter />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
