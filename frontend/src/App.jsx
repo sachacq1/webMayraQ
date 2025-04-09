9c016c71ed9621dbc4297f15f44a3e679641b876
sachacq1 <sachacq1@gmail.com>
2025-04-09 12:28:37 -0300

    home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,4 @@
-import { useAuth } from "./context/authContext.jsx";
-import { AdminPacientesList } from "./views/Admin/dashboard.jsx";
-
-import { Routes, Route } from "react-router-dom";
-//
-import Login from "../views/login/login";
-import Register from "../views/register/register";
+import { Route, Router } from "react-router-dom";
 import Inicio from "../views/inicio/inicio";
 import Servicios from "../views/servicios/servicios";
 import SobreMi from "../views/sobreMi/sobreMi";
@@ -16,12 +10,14 @@ const App = () => {
 
   return (
     <>
-      <Header />
-      <Route path="/inicio" element={<Inicio />} />
-      <Route path="/servicios" element={<Servicios />} />
-      <Route path="/sobreMi" element={<SobreMi />} />
-      <Route path="/contacto" element={<Contacto />} />
-      <Footer />
+      <Router>
+        <Header />
+        <Route path="/inicio" element={<Inicio />} />
+        <Route path="/servicios" element={<Servicios />} />
+        <Route path="/sobreMi" element={<SobreMi />} />
+        <Route path="/contacto" element={<Contacto />} />
+        <Footer />
+      </Router>
     </>
 
   )
