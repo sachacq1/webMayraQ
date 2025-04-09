d67952b3d1fc786ba074c44181b7db44283558f9
sachacq1 <sachacq1@gmail.com>
2025-04-09 18:03:54 -0300

    camb rooout

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -10,7 +10,7 @@ import Register from "../views/register/register.jsx";
 import LayoutPublico from "../Layout/LayoutPublico.jsx";
 import Landing from "../Layout/Landing.jsx";
 import Panel from "../views/panel/panel.jsx";
-import AdminPacientesList from "../views/Admin/AdminPacientesList.jsxx";
+import AdminPacientesList from "../views/Admin/AdminPacientesList.jsx";
 import AdminPacientesAdd from "../views/Admin/AdminPacienteAdd.jsx";
 
 const AppRouter = () => {
