797a8e46e57677cf09c6dadfa3354239c5a3b591
sachacq1 <sachacq1@gmail.com>
2025-04-09 17:59:33 -0300

    appr

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 
+import { AppRouter } from "./router/Router.jsx";
 import { AdminPacientesList } from "./views/Admin/dashboard.jsx";
 import Panel from "./views/panel/panel.jsx";
+import { AppRouter } from "./router/Router.jsx";
 
 const App = () => {
 
   return (
     <>
-      <div className="container mt-4">
-        <h1>Bienvenido al Panel Privado</h1>
-        <p>Acá iría tu dashboard o lo que quieras que solo vea la psicóloga.</p>
-        <Panel />
-      </div>
+
+      <AppRouter />
+
     </>
   );
 };
